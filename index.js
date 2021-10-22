b8aff999d66e38cdabaa30404deced5a21b793b3
Paul Fernandez <paul.fernandez@automattic.com>
2021-10-22 01:22:39 -0700

    recurse WIP

--- index.js
+++ index.js
@@ -4,14 +4,23 @@
 
 const log = console.log
 
-const forEach = function(list, f) { list.forEach(x => f(x)) }
+const each = (list, f) => list.forEach(x => f(x))
 
 const push = (x, list) => list.push(x)
 
-const apply = (f, list) => Reflect.apply(f, undefined, list)
+const apply = (f, list) => f.apply(undefined, list)
 
-const add = ({ a, b }) => a + b
+const reverse = list => [...list].reverse()
 
+const add = (a, b) => a + b
+
+const isFunction = x => typeof x === 'function'
+
+const isData = x => !isFunction(x)
+
+const isList = x => Array.isArray(x)
+
+const self = x => x
 
 ////////////////////////////////////////////////////////////////////////////////
 // Core
@@ -23,32 +32,21 @@ const history = [store]
 
 const assign = x => (push(x, history), store = { ...store, ...x })
 
-const evaluate = ([f, ...args]) => apply(f, args) // TODO: Recurse
+const evaluate = ([f, ...rest]) => isFunction(f) ? apply(f, rest) : [f, ...rest]
+
+// FIXME: Start at the leaves
+const recurse = list =>
+  (each(list, x => isList(x) && evaluate(list) && recurse(x)))
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Application
 
 
-apply(
-  forEach, [[
+recurse(
+  [log,
     1,
-    apply(
-      add, [{
-        a: 1,
-        b: 1 }]),
-    apply(
-      add, [{
-        a: 1,
-        b: apply(
-          add, [{
-            a: 1,
-            b: 1 }])}])],
-  log])
-
-evaluate([
-  log,
-  assign({ foo: 'bar' }),
-  assign({ baz: 'qux' }),
-  history])
+    [add, 1, 1],
+    [add, 1,
+      [add, 1, 1]]])
 
