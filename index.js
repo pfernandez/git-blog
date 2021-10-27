b917a1dc800ef598223e6deb4d05bdca22285e98
Paul Fernandez <paul.fernandez@automattic.com>
2021-10-26 22:37:04 -0700

    WIP basic McCarthy defs

--- index.js
+++ index.js
@@ -1,52 +1,41 @@
-////////////////////////////////////////////////////////////////////////////////
-// Utilities
 
+exists = x => typeof x !== undefined
+list = x => Array.isArray(x)
+obj = x => typeof x === 'object' && !list(x) && x !== null // = associative list
+atom = x => exists(x) && !list(x)
+nil = []
+rest = ([_, ...x]) => x.length ? x : nil  // cdr
 
-const log = console.log
+// TODO: Align the definitions with the paper
+first = ([x]) => list(x) && x  // car
+//combine = (x, y)  // cons
+func = x => typeof x === 'function'
+sfunc = x => list(x) && func(first(x))
+among = (a, f) => a.some(f)
+apply = (f, x) => f.apply(undefined, list(x) ? x : [x])
+evaluate = x => flist(x) ? apply(first(x), rest(x)) : x
 
-const each = (list, f) => list.forEach(x => f(x))
+// Just helpers
+each = (a, f) => a.forEach(x => f(x))  // could use cad-cdr recursion but...
+leaf = x => list(x) && !among(x, expr)
+last = a => first(a.slice(-1))
+log = (...x) => (console.log(...x), last(x))
+add = (a, b) => a + b
 
-const push = (x, list) => list.push(x)
-
-const apply = (f, list) => f.apply(undefined, list)
-
-const reverse = list => [...list].reverse()
-
-const add = (a, b) => a + b
-
-const isFunction = x => typeof x === 'function'
-
-const isData = x => !isFunction(x)
-
-const isList = x => Array.isArray(x)
-
-const self = x => x
-
-////////////////////////////////////////////////////////////////////////////////
-// Core
-
-
-var store = {}
-
-const history = [store]
-
-const assign = x => (push(x, history), store = { ...store, ...x })
-
-const evaluate = ([f, ...rest]) => isFunction(f) ? apply(f, rest) : [f, ...rest]
-
-// FIXME: Start at the leaves
-const recurse = list =>
-  (each(list, x => isList(x) && evaluate(list) && recurse(x)))
-
-
-////////////////////////////////////////////////////////////////////////////////
-// Application
-
+// Figure out and replace evaluate
+recurse = a =>
+  isLeaf(a)
+    ? log('*', a, '---->', evaluate(a))
+    : each(a, x =>
+        log('*', x)
+        && isFunctionList(x)
+        && log('-----')
+        && recurse(x))
 
 recurse(
   [log,
     1,
     [add, 1, 1],
-    [add, 1,
-      [add, 1, 1]]])
+    [add, 1, [add, 1, 1]]])
 
+log(1, 2, 3, 4)
