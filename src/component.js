f50a19a6fb4ada3afabf4b4206efc62fea8536a4
Paul Fernandez <paul.fernandez@automattic.com>
2021-11-14 23:27:36 -0800

    Recurse without create

--- src/component.js
+++ src/component.js
@@ -7,22 +7,23 @@ export const create = (tagName, x, ...nodes) => {
     : append(createElement(tagName), x ?? '', ...nodes)
   const { id, localName } = element
   const liveElement = querySelector(id ? `#${id}` : localName)
-  liveElement && log('replacing', localName) && replaceWith(liveElement, element)
+  // TODO: Should only attach first element. Could diff x and nodes.
+  liveElement && log(localName, x, nodes) && replaceWith(liveElement, element)
   return element
 }
 
 
-export const {main, h1, pre, button, fragment} = reduce(
-  ['main', 'h1', 'pre', 'button'],
+export const {main, h1, div, pre, button, fragment} = reduce(
+  ['main', 'h1', 'div', 'pre', 'button'],
   (functions, tagName) =>
     ({...functions, [tagName]: (x, ...nodes) => create(tagName, x, ...nodes)}),
   {fragment: (...nodes) => append(createFragment(), ...nodes)})
 
 
-const counter = (text = 'Increment', count = 0) =>
-  create('counter', { id: 'counter-1' },
+export const counter = (id = 'foo', text = 'Increment', count = 0) =>
+  div({ id },
     pre({innerHTML: count, style: {fontSize: '4em'}}),
-    button({onclick: () => counter(text, count + 1)}, text))
+    button({onclick: () => counter(id, text, count + 1)}, text))
 
 
 const tree = () =>
