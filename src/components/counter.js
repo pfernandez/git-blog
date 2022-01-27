b85226d8cc54b29ee1d94e0426105846435789ad
Paul Fernandez <paul.fernandez@automattic.com>
2022-01-26 22:08:25 -0800

    Update only the root element

--- src/components/counter.js
+++ src/components/counter.js
@@ -1,16 +1,13 @@
-// TODO: Without an ID assigned to every element, every div, pre, and button on
-// the page will be rerendered with the arguments passed in here as soon as
-// counter is called.
+
 const counter = (count, id) =>
   div({ id },
     pre(
-      { id,
-        style: { fontSize: '4em',
+      { style: { fontSize: '4em',
                  margin: '20px 0' } },
       count),
     button(
-      { id,
-        onclick: () => counter(count + 1, id) },
+      { onclick: () => counter(count + 1, id) },
       'Increment'))
 
 export default counter
+
--- src/expressive/elements.js
+++ src/expressive/elements.js
@@ -1,26 +1,9 @@
-import { log } from './functions.js'
-
-const tagNames = [
-  'a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base',
-  'bdi', 'bdo', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption',
-  'cite', 'code', 'col', 'colgroup', 'data', 'datalist', 'dd', 'del', 'details',
-  'dfn', 'dialog', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption',
-  'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head',
-  'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd',
-  'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'menu', 'meta',
-  'meter', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output',
-  'p', 'param', 'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's',
-  'samp', 'script', 'section', 'select', 'slot', 'small', 'source', 'span',
-  'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td',
-  'template', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr',
-  'track', 'u', 'ul', 'var', 'video', 'wbr'
-]
 
 const assignProperties = (baseElement, properties) =>
   Object.entries(properties).reduce((el, [k, v]) =>
     (el[k] = v,
     typeof v === 'object' && !Array.isArray(v)
-      && assignProperties(el[k], selector, v),
+      && assignProperties(el[k], v),
     el),
   baseElement)
 
@@ -38,7 +21,7 @@ const selector = (tagName, { id, className }) =>
       : className ? `.${className.split(' ').filter(s => s).join('.')}`
         : '')
 
-const disambiguate = (tagName, x, childNodes) =>
+const withProperties = (tagName, x, childNodes) =>
   ({ tagName,
      ...typeof x === 'object' && !(x instanceof Node)
        ? { childNodes,
@@ -58,11 +41,13 @@ const baseElement = tagName =>
     ? 'html' === tagName ? document.documentElement : document[tagName]
     : document.createElement(tagName)
 
-const isRoot = args => log(args) && 0 // TODO
+const isRootElement = args => args.childNodes.some(
+  node => node instanceof Element && node.parentNode === null)
 
-const liveElement =
+const createLiveElement =
   ({ childNodes, properties, selector, tagName }) =>
-    isRoot({ childNodes, properties, selector, tagName })
+    document.readyState === 'complete'
+    && isRootElement({ childNodes, properties, selector, tagName })
       ? update(selector, properties, childNodes)
       : replaceSubtree(baseElement(tagName), properties, childNodes)
 
@@ -80,15 +65,32 @@ const liveElement =
  * @returns HTMLElement
  */
 export const element = (tagName, nodeOrProperties, ...nodes) =>
-  liveElement(disambiguate(tagName, nodeOrProperties, nodes))
+  createLiveElement(withProperties(tagName, nodeOrProperties, nodes))
 
 export const el = element
+
+const tagNames = [
+  'a', 'abbr', 'address', 'area', 'article', 'aside', 'audio', 'b', 'base',
+  'bdi', 'bdo', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption',
+  'cite', 'code', 'col', 'colgroup', 'data', 'datalist', 'dd', 'del', 'details',
+  'dfn', 'dialog', 'div', 'dl', 'dt', 'em', 'embed', 'fieldset', 'figcaption',
+  'figure', 'footer', 'form', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head',
+  'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'img', 'input', 'ins', 'kbd',
+  'label', 'legend', 'li', 'link', 'main', 'map', 'mark', 'menu', 'meta',
+  'meter', 'nav', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output',
+  'p', 'param', 'picture', 'pre', 'progress', 'q', 'rp', 'rt', 'ruby', 's',
+  'samp', 'script', 'section', 'select', 'slot', 'small', 'source', 'span',
+  'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td',
+  'template', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr',
+  'track', 'u', 'ul', 'var', 'video', 'wbr'
+]
 
 const defaultElements = tagNames.reduce(
   (functions, tagName) =>
     ({ ...functions,
        [tagName]: (nodeOrProperties, ...nodes) =>
          element(tagName, nodeOrProperties, ...nodes) }),
+
   { doctype: (qualifiedName, publicId = '', systemId = '') =>
     document.implementation
       .createDocumentType(qualifiedName, publicId, systemId),
