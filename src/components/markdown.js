3262b25c01a4bcf73b4f1db2a51d57d1629cfbd1
Paul Fernandez <paul4nandez@gmail.com>
2022-06-25 20:23:20 -0700

    Cleanup

--- src/components/markdown.js
+++ src/components/markdown.js
@@ -12,29 +12,36 @@ const parseCodeBlock = (str, language) =>
 
 const config = { html: true,
                  linkify: true,
-                 typographer: true,
                  highlight: parseCodeBlock }
 
 const md = (markdown = '', props = {}) =>
   createElement('md', { innerHTML: markdownit(config).render(markdown),
                         ...props })
 
-// Because inline scripts have already been run, rerendering them (i.e. when
-// navigating away from, then back to content without a page refresh) will cause
-// redecalaration errors for any `const` or `let` statements. To work around
-// this we suppress during all script injections after the initial load.
+/**
+ * Because inline scripts have already been run, rerendering them (i.e. when
+ * navigating away from, then back to content without a page refresh) will cause
+ * redecalaration errors for any `const` or `let` statements. To work around
+ * this we suppress errors during all script injections after the initial load
+ * by causing `window.onerror` to return `false`.
+ */
 let loaded = false
 const temp = window.onerror
+
+const replaceScripts = el =>
+  el.querySelectorAll('script, .language-live-js')
+    .forEach(s => s.tagName.toLowerCase() === 'script'
+      ? s.replaceWith(script(s.innerText))
+      : s.after(script(s.innerText)))
+
 const injectScripts = el =>
   (window.onerror = () => loaded,
-   el.querySelectorAll('script, .language-live-js')
-     .forEach(s => s.tagName.toLowerCase() === 'script'
-       ? s.replaceWith(script(s.innerText))
-       : s.after(script(s.innerText))),
+   replaceScripts(),
    window.onerror = temp,
    loaded = true)
 
-const injectMarkdown = el => (update(el), injectScripts(el))
+const injectMarkdown = el =>
+  (update(el), injectScripts(el))
 
 const renderMarkdown = (markdown, props) =>
   (fetch(markdown).then(response => response.text())
