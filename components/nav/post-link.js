a51313946c571b12574f0914e6d65cdf715b3a97
Paul Fernandez <paul4nandez@gmail.com>
2023-03-10 17:06:11 -0800

    Remove basePath function

--- components/nav/post-link.js
+++ components/nav/post-link.js
@@ -5,18 +5,14 @@ import {setUrl} from '../../lib/expressive/dom/utils.js'
 
 const renderPost = () => update(post())
 
+// FIXME: Not rendering when the back button is clicked.
 on('popstate', renderPost)
 
-const basePath = (segments = location.pathname.split('/')) =>
-  segments.slice(0, segments.length - 1).join('/')
-
-// TODO: Append/remove URL segments as the folders are navigated with the
-// sidebar links. Add a link (with back icon) to the parent directory.
-//
-// * The rendered post should update with sidebar navigation.
+// TODO: 
+// * Add a sidebar link (with back icon) to the parent directory.
 // * The URL should always reflect the actual path to the markdown file in the
 //   project.
-const createLink = (linkText, path = basePath() + '/' + linkText) =>
+const createLink = (linkText, path = location.pathname + linkText) =>
   a({onclick: () =>
     linkText.endsWith('/')  // the link was to a folder
       ? log('navigating and listing', path + 'index.js',
