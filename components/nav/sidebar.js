295bdc16dbf15b118715c61892b1d6aeb8969860
Paul Fernandez <paul4nandez@gmail.com>
2023-03-12 22:57:59 -0700

    Parent directory links WIP

--- components/nav/sidebar.js
+++ components/nav/sidebar.js
@@ -1,11 +1,41 @@
-import postLink from './post-link.js'
+import post from '../post.js'
+import sidebar from './sidebar.js'
+import {update} from '../../lib/expressive/dom/elements.js'
+import {setUrl} from '../../lib/expressive/dom/utils.js'
+
+const renderPost = () => update(post())
+
+// FIXME: Not rendering when the back button is clicked.
+on('popstate', renderPost)
+
+const fullPath = linkText =>
+  location.pathname + linkText
+
+const createLink = (linkText, path = fullPath(linkText)) =>
+  a({onclick: () =>
+    linkText.endsWith('/')  // the link was to a folder
+      ? log('navigating and listing', path + 'index.js',
+            setUrl(path, () => update(sidebar(path + 'index.js'))))
+
+      : log('rendering', path,
+            setUrl(path, renderPost))},
+    linkText)
 
 const linkText = fileName =>
   replace(fileName, '.md', '')
 
-const list = items =>
-  ul(map(items, name =>
-    li(postLink(linkText(name)))))
+const parentDirectory = (path = location.pathname) =>
+  path.endsWith('/')
+    ? path.split('/').slice(-3)[0]
+    : path.split('/').slice(-2)[0]
+
+const list = (items, parent = parentDirectory()) =>
+  ul(when(parent !== '/',
+          li(a({onclick: () => setUrl(fullPath(parent), renderPost)},
+               i({className: 'arrow left'}),
+               parentDirectory()))),
+     map(items, name =>
+       li(createLink(linkText(name)))))
 
 const updateList = ({default: items}) => update(list(items))
 
