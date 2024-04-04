f11330ed0a8d7417d429509c28b9e8faefaef09f
Paul Fernandez <paul4nandez@gmail.com>
2024-04-03 21:19:23 -0700

    Trying without update function

--- components/nav/sidebar.js
+++ components/nav/sidebar.js
@@ -1,41 +1,34 @@
 import post from '../post.js'
 import sidebar from './sidebar.js'
-import {update} from '../../lib/expressive/dom/elements.js'
 import {setUrl} from '../../lib/expressive/dom/utils.js'
 
-const renderPost = () => update(post())
-
 // FIXME: Not rendering when the back button is clicked.
-on('popstate', renderPost)
+on('popstate', post)
 
 const fullPath = linkText =>
   location.pathname + linkText
 
-const createLink = (linkText, path = fullPath(linkText)) =>
-  a({onclick: () =>
+const link = (linkText, path = fullPath(linkText)) =>
+  a({id: replace(linkText, '/', ''), onclick: () =>
     linkText.endsWith('/')  // the link was to a folder
       ? log('navigating and listing', path + 'index.js',
-            setUrl(path, () => update(sidebar(path + 'index.js'))))
+            setUrl(path, () => sidebar(path + 'index.js')))
 
       : log('rendering', path,
-            setUrl(path, renderPost))},
+            setUrl(path, post))},
     linkText)
-
-const linkText = fileName =>
-  replace(fileName, '.md', '')
 
 const parentDirectory = (path = location.pathname) =>
   path.split('/').slice(path.endsWith('/') ? -3 : -2).join('/')
 
 const list = (items, parent = parentDirectory()) =>
   ul(when(parent !== '/',
-          li(a({onclick: () => setUrl(log(parent, '/' + parent))}, // renderPost)},
+          li(a({id: 'first-link', onclick: () => setUrl(log(parent, '/' + parent))}, // post)},
                i({className: 'arrow left'}),
                parent))),
      map(items, name =>
-       li(createLink(linkText(name)))))
-
-const updateList = ({default: items}) => update(list(items))
+       li(link(replace(name, '.md', '')))))
 
 export default (index = '/index.js') =>
-  (import(log('Fetching', index)).then(updateList), list([]))
+  (import(log('Fetching', index)).then(({default: items}) =>
+    list(items)), list([]))
