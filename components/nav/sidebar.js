18b024c98a63d43e4fd35c8953ae050d4b2fb7f8
Paul Fernandez <paul4nandez@gmail.com>
2024-05-05 17:25:24 -0700

    Revert "Revert "Revert "Trying without update function"""
    This reverts commit fca060094993267da1528bd5764e485ac0788bb1.

--- components/nav/sidebar.js
+++ components/nav/sidebar.js
@@ -1,34 +1,41 @@
 import post from '../post.js'
 import sidebar from './sidebar.js'
+import {update} from '../../lib/expressive/dom/elements.js'
 import {setUrl} from '../../lib/expressive/dom/utils.js'
 
+const renderPost = () => update(post())
+
 // FIXME: Not rendering when the back button is clicked.
-on('popstate', post)
+on('popstate', renderPost)
 
 const fullPath = linkText =>
   location.pathname + linkText
 
-const link = (linkText, path = fullPath(linkText)) =>
-  a({id: replace(linkText, '/', ''), onclick: () =>
+const createLink = (linkText, path = fullPath(linkText)) =>
+  a({onclick: () =>
     linkText.endsWith('/')  // the link was to a folder
       ? log('navigating and listing', path + 'index.js',
-            setUrl(path, () => sidebar(path + 'index.js')))
+            setUrl(path, () => update(sidebar(path + 'index.js'))))
 
       : log('rendering', path,
-            setUrl(path, post))},
+            setUrl(path, renderPost))},
     linkText)
+
+const linkText = fileName =>
+  replace(fileName, '.md', '')
 
 const parentDirectory = (path = location.pathname) =>
   path.split('/').slice(path.endsWith('/') ? -3 : -2).join('/')
 
 const list = (items, parent = parentDirectory()) =>
   ul(when(parent !== '/',
-          li(a({id: 'first-link', onclick: () => setUrl(log(parent, '/' + parent))}, // post)},
+          li(a({onclick: () => setUrl(log(parent, '/' + parent))}, // renderPost)},
                i({className: 'arrow left'}),
                parent))),
      map(items, name =>
-       li(link(replace(name, '.md', '')))))
+       li(createLink(linkText(name)))))
+
+const updateList = ({default: items}) => update(list(items))
 
 export default (index = '/index.js') =>
-  (import(log('Fetching', index)).then(({default: items}) =>
-    list(items)), list([]))
+  (import(log('Fetching', index)).then(updateList), list([]))
--- index.js
+++ index.js
@@ -2,6 +2,6 @@ export default
 
 ['home.md',
  'about.md',
- 'elements.md',
+  'elements.md',
  'posts/']
 
